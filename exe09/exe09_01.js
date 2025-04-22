3202f7e290386bf4798c8b959bbb92735b7d0edc
Gustavo <gustavo.g.santos14@aluno.senai.br>
2025-04-22 10:22:29 -0300

    exercicios09

---
+++ exe09/exe09_01.js
@@ -0,0 +1,6 @@
+function formatarData (dia, mês, ano){
+    data = `o dia é ${dia} o mes é ${mês} e o ano é ${ano}`
+    return data
+}
+valorRetornado = formatarData(28,2,2009)
+console.log(valorRetornado)
